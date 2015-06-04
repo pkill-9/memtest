6c8d8897aa9da4c67f87fa901ec1b0b0e34fb5c1
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-06-04 16:14:11 +1000

    Added function prototypes for functions to print strings and ints on the screen via the BIOS.

---
+++ output.h
@@ -0,0 +1,24 @@
+/**
+ *  Functions for printing data on the screen via BIOS interrupts.
+ *
+ *  Since our code is a bootsector, there is no cushy C library printf
+ *  for printing to the screen. Fortunately, the BIOS provides some
+ *  abstraction for putting a character on the screen, so we don't have to
+ *  go stuffing around with video memory, but we do need to handle 
+ *  printing both strings and ints.
+ */
+
+#ifndef _MEMTEST_OUTPUT_H
+#define _MEMTEST_OUTPUT_H
+
+/**********************************************************/
+
+void print_string (const char *string);
+void print_int_hex (uint32_t value);
+void print_long_hex (uint32_t lsb, uint32_t msb);
+
+/**********************************************************/
+
+#endif // _MEMTEST_OUTPUT_H
+
+/** vim: set ts=4 sw=4 et : */
