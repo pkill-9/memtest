a2399ef994e7e25194536851f3eeb3e247b0ae2b
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-06-09 18:39:34 +1000

    Added the assembly language function that invokes BIOS interrupts for C programs.

---
+++ bios.s
@@ -0,0 +1,91 @@
+/**
+ *  Function that allows C programs to make use of BIOS services.
+ */
+
+.code16
+.include "const.s"
+
+.section .text
+
+/**
+ *  bios_interrupt (struct cpu_registers *r, int code)
+ *
+ *  Raise BIOS interrupt code, with the values specified in the registers
+ *  struct in the general purpose registers. Once the interrupt returns,
+ *  values of the registers will be stored back into the struct for the
+ *  caller to analyse.
+ */
+    .globl _bios_interrupt
+_bios_interrupt:
+    push    %ebp
+    mov     %esp, %ebp
+    pusha
+
+# get the values for the general purpose registers from the struct.
+    mov     12(%ebp), %eax
+    mov     EBX(%eax), %ebx
+    mov     ECX(%eax), %ecx
+    mov     EDX(%eax), %edx
+    mov     ES(%eax), %es
+    mov     ESI(%eax), %esi
+    mov     EDI(%eax), %edi
+    mov     EAX(%eax), %eax
+
+# test which interrupt we are to raise.
+    cmp     $0x16, 8(%ebp)
+    je      int_16
+    cmp     $0x15, 8(%ebp)
+    je      int_15
+    cmp     $0x13, 8(%ebp)
+    je      int_13
+    cmp     $0x10, 8(%ebp)
+    je      int_10
+    jmp     error
+
+# Interrupt 0x16, keyboard services, eg read next key press.
+int_16:
+    int     $0x16
+    jmp     restore
+
+# Interrupt 0x15, Misc, including fetching a detailed memory map.
+int_15:
+    int     $0x15
+    jmp     restore
+
+# Interrupt 0x13, Disk services. Read blocks, write blocks
+int_13:
+    int     $0x13
+    jmp     restore
+
+# Interrupt 0x10, Display services. Print characters, move cursor.
+int_10:
+    int     $0x10
+    jmp     restore
+
+error:
+    mov     $-1, %eax
+
+# fall through to restore.
+
+restore:
+    push    %eax
+    mov     12(%ebp), %eax
+    mov     %ebx, EBX(%eax)
+    mov     %ecx, ECX(%eax)
+    mov     %edx, EDX(%eax)
+    mov     %esi, ESI(%eax)
+    mov     %edi, EDI(%eax)
+    mov     %es, ES(%eax)
+    pushf
+    pop     %ebx
+    mov     %ebx, EFLAGS(%eax)
+    popl    %ebx
+    mov     %ebx, EAX(%eax)
+
+    popa
+
+    mov     %ebp, %esp
+    popl    %ebp
+    ret
+
+# vim: ts=4 sw=4 et
---
+++ const.s
@@ -0,0 +1,19 @@
+/**
+ *  Constants for the assembly language source code.
+ */
+
+/**
+ *  These constants define the offsets of certain fields within the struct
+ *  cpu_registers.
+ */
+
+.equ EAX, 0
+.equ EBX, 4
+.equ ECX, 8
+.equ EDX, 12
+.equ ESI, 16
+.equ EDI, 20
+.equ ES, 24
+.equ EFLAGS, 28
+
+# vim: ts=4 sw=4 et
