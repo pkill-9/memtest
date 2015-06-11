502b2f94125a41313833755889c269a2390d89f1
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-06-11 17:27:43 +1000

    Fixed an issue with the bootsector where NUMSECTORS was not being pointed to the correct memory address.
    To fix it, I changed the way NUMSECTORS was defined, to be defined in
    start.s and mapped to an address by the link script, rather than being
    defined in the link script.
    
    As of this commit, the bootsector works, as tested with virtualbox, but
    the second stage crashes horribly.

--- bootsector/start.s
+++ bootsector/start.s
@@ -27,9 +27,20 @@ _start:
     mov     $0x00, %dh          # head = 0
     mov     $0x02, %cl          # sector = 2
     mov     $0x02, %ah          # ah=0x2, read sectors.
+    mov     $0x7E00, %bx
     mov     NUMSECTORS, %al
     int     $0x13
 
     jmp     0x7E00
+
+
+.section .params
+
+/**
+ *  Number of sectors to load. This value will be filled in by the program
+ *  that copies memtest onto the disk image.
+ */
+NUMSECTORS:
+.byte
 
 /** vim: set ts=4 sw=4 et : */
