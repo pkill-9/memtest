0777f4c699cd7247e911b18b59881771b84eda26
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-06-11 12:22:48 +1000

    Decided to simplify the bootsector, since the second stage will be in contiguous sectors immediately after the bootsector.
    This means that it is safe to assume values of 0 for head and sector, so
    there is no need to go through the routine of obtaining drive geometry and
    calculating the disk address etc.
    
    Deleted assembly language subroutines from the bootsector, since they are
    now redundant.

--- bootsector/start.s
+++ bootsector/start.s
@@ -20,52 +20,16 @@ _start:
     mov     %bp, %sp
     sti
 
-    pushl   NUMSECTORS
-    call    _cboot
-
-/**
- *  void get_drive_parameters (uint16_t *cx, uint8_t *dh)
- *
- *  Queries the BIOS for drive parameters. After the interrupt, the cx
- *  register contains the number of cylinders and sectors and dh has the
- *  number of heads. It is left to the caller to unpack these values.
- */
-    .globl  _get_drive_parameters
-_get_drive_parameters:
-    push    %ebp
-    mov     %esp, %ebp
-
-    mov     $0x08, %ah
-    mov     $0x80, %dl
+# The first sector of the second stage is going to be immediately after
+# the bootsector, so it is pretty safe to assume that sector=2, cylinder=0
+# and head=0.
+    mov     $0x00, %ch          # cylinder = 0
+    mov     $0x00, %dh          # head = 0
+    mov     $0x02, %cl          # sector = 2
+    mov     $0x02, %ah          # ah=0x2, read sectors.
+    mov     NUMSECTORS, %al
     int     $0x13
 
-    mov     8(%ebp), %ebx
-    mov     %dh, (%bx)
-    mov     12(%ebp), %ebx
-    mov     %cx, (%bx)
-
-    pop     %ebp
-    ret
-
-/**
- *  void read_sectors (uint16_t cx, uint16_t dx, void *buffer)
- *
- *  Read a sector from the disk into memory. cx is the value to be put
- *  in the cx register for the interrupt, and should be the cylinder and
- *  sector index. The dx register should have the head number.
- */
-    .globl  _read_sectors
-read_sectors:
-    push    %ebp
-    mov     %esp, %ebp
-
-    mov     8(%ebp), %ebx
-    mov     12(%ebp), %dx
-    mov     14(%ebp), %cx
-    mov     $0x0201, %ax
-    int     $0x13
-
-    pop     %ebp
-    ret
+    jmp     0x7E00
 
 /** vim: set ts=4 sw=4 et : */
